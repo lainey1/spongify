0414197dbad931e6288bf2d0c5e71ff5c607ed3d
Quonnaq0711 <shaquonna.lynna@gmail.com>
2024-12-20 14:14:31 -0500

    create_restaurant

--- app/api/restaurant_routes.py
+++ app/api/restaurant_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
-from app.models import Restaurant
+from flask import Blueprint, jsonify, redirect, render_template, flash
+from app.models import Restaurant, db
+from app.forms import NewRestaurant
 
 restaurant_routes = Blueprint('restaurants', __name__)
 
@@ -20,3 +21,46 @@ def restaurant(id):
     """
     restaurant = Restaurant.query.get(id)
     return restaurant.to_dict()
+
+@restaurant_routes.route('/restuarants', methods=['POST'])
+def create_restaurant():
+    """
+    Query to add a restaurant to the DB
+    """
+    form = NewRestaurant()
+
+    if form.validate_on_submit():
+        new_restaurant = restaurants(
+            name = form.name.data,
+            address = form.address.data,
+            city = form.city.data,
+            state = form.state.data,
+            country = form.country.data,
+            phone_number = form.phone_number.data,
+            email = form.email.data,
+            website = form.website.data,
+            cuisine = form.cuisine.data,
+            price_point = form.price_point.data,
+            description = form.description.data,
+            monday_hours = form.monday_hours.data,
+            tuesday_hours = form.tuesday_hours.data,
+            wednesday_hours = form.wednesday_hours.data,
+            thursday_hours = form.thursday_hours.data,
+            friday_hours = form.friday_hours.data,
+            saturday_hours = form.saturday_hours.data,
+            sunday_hours = form.sunday_hours.data
+            )
+        
+
+        db.session.add(new_restaurant)
+        db.session.commit()
+        flash("Restaurant added successfully!", "success")
+        return redirect('/')
+    
+    flash("Bad Data, please check your inputs", "error")
+    return('/restaurants')
+
+    
+
+
+    
