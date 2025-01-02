51d82fc73e62db21a5e01fb9a85afe5664717a01
rchinson <rhinson2010@gmail.com>
2025-01-02 14:15:56 -0800

    render test

---
+++ react-vite/src/components/UserProfile/UserProfile.jsx
@@ -0,0 +1,21 @@
+// import { useDispatch, useSelector } from 'react-redux';
+import './UserProfile.css';
+// import { useParams } from "react-router-dom";
+
+
+function UserProfile() {
+
+    // const { userId } = useParams();
+    // const dispatch = useDispatch();
+
+    
+
+
+    return (
+        <h1>User Profile Page</h1>
+    )
+
+}
+
+
+export default UserProfile
