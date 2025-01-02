e2701b7fb3b356e01ae3bf69f7c6b309cb71faf6
lainey1 <lainey.d.paddock@gmail.com>
2025-01-01 21:30:45 -0800

    added addition reservartion and review images seed data

--- app/seeds/reservations.py
+++ app/seeds/reservations.py
@@ -1,59 +1,4 @@
 from datetime import datetime
-
-# from app.models import db, Reservation, environment, SCHEMA
-# from sqlalchemy.sql import text
-
-
-
-# # Adds a demo user, you can add other users here if you want
-# def seed_reservations():
-#     res1 = Reservation(
-#             restaurant_id = 1,
-#             user_id = 1,
-#             date = '2025-01-01 10:30:00',
-#             party_size = 3
-#         )
-#     res2 = Reservation(
-#             restaurant_id = 2,
-#             user_id = 2,
-#             date = '2025-02-11 13:45:00',
-#             party_size = 3
-#         )
-#     res3 = Reservation(
-#             restaurant_id = 3,
-#             user_id = 3,
-#             date = '2025-11-22 06:30:00',
-#             party_size = 3
-#         )
-#     res4 = Reservation(
-#             restaurant_id = 4,
-#             user_id = 4,
-#             date = '2025-03-11 08:00:00',
-#             party_size = 3
-#         )
-
-#     db.session.add(res1)
-#     db.session.add(res2)
-#     db.session.add(res3)
-#     db.session.add(res4)
-#     db.session.commit()
-
-
-# # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
-# # have a built in function to do this. With postgres in production TRUNCATE
-# # removes all the data from the table, and RESET IDENTITY resets the auto
-# # incrementing primary key, CASCADE deletes any dependent entities.  With
-# # sqlite3 in development you need to instead use DELETE to remove all data and
-# # it will reset the primary keys for you as well.
-# def undo_reservations():
-#     if environment == "production":
-#         db.session.execute(f"TRUNCATE table {SCHEMA}.reservations RESTART IDENTITY CASCADE;")
-#     else:
-#         db.session.execute(text("DELETE FROM reservations"))
-
-#     db.session.commit()
-
-
 from app.models import db, Reservation, environment, SCHEMA
 from sqlalchemy.sql import text
 
@@ -62,13 +7,31 @@ from sqlalchemy.sql import text
 def seed_reservations():
     res1 = Reservation(
            id = 1,
-           restaurant_id = 4,
-           user_id = 3,
+           restaurant_id = 3,
+           user_id = 1,
            date=datetime.strptime('2024-12-20 13:45:00', '%Y-%m-%d %H:%M:%S'),
            party_size = 4,
         )
 
+    res2 = Reservation(
+        id=2,
+        restaurant_id=2,
+        user_id=3,
+        date=datetime.strptime('2024-12-21 18:00:00', '%Y-%m-%d %H:%M:%S'),
+        party_size=2,
+    )
+
+    res3 = Reservation(
+        id=3,
+        restaurant_id=1,
+        user_id=2,
+        date=datetime.strptime('2024-12-22 19:30:00', '%Y-%m-%d %H:%M:%S'),
+        party_size=6,
+    )
+
     db.session.add(res1)
+    db.session.add(res2)
+    db.session.add(res3)
     db.session.commit()
 
 
--- app/seeds/review_images.py
+++ app/seeds/review_images.py
@@ -3,13 +3,31 @@ from sqlalchemy.sql import text
 
 #Add demo review images
 def seed_review_image():
-    review_image = ReviewImage(
+    review_image1 = ReviewImage(
         review_id = 1,
         user_id = 1,
         url = 'https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=600',
     )
+    review_image2 = ReviewImage(
+        review_id = 1,
+        user_id = 1,
+        url = 'https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=600',
+    )
+    review_image3 = ReviewImage(
+        review_id = 2,
+        user_id = 2,
+        url = 'https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=600',
+    )
+    review_image4 = ReviewImage(
+        review_id = 3,
+        user_id = 3,
+        url = 'https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=600',
+    )
 
-    db.session.add(review_image)
+    db.session.add(review_image1)
+    db.session.add(review_image2)
+    db.session.add(review_image3)
+    db.session.add(review_image4)
     db.session.commit()
 
 
